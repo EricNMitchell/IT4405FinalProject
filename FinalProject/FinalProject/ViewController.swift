9a919554b407c23ac12efb50f85a9ab27493b9ba
Eric <emitchell008@gmail.com>
2019-05-10 18:01:12 -0500

    UI layout set

---
+++ FinalProject/FinalProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FinalProject
+//
+//  Created by Eric Mitchell on 5/10/19.
+//  Copyright © 2019 Eric Mitchell. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
